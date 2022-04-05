bac2a29cb140a28ff5fa9e8b041f7a647205b2fb
MeRaksha <95466002+MeRaksha@users.noreply.github.com>
2022-04-05 22:03:18 +0530

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,17 @@ module.exports = {
       }, {
         test: /\.(png|jpeg|jpg|svg|gif)$/i,
         type: 'asset/resource'
-      }
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }, {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
     ]
   }
 }
