425878524e10dc5beba014085338329088d8ab8f
MeRaksha <95466002+MeRaksha@users.noreply.github.com>
2022-04-05 22:06:57 +0530

    load data files like xml, csv demo added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {join} from 'lodash-es';
 import './style.css';
 import IconBird from './IconBird.png';
+import JsonData from './data.json';
+import XmlData from './data.xml';
+import CsvData from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -10,6 +13,9 @@ function component() {
   const image = new Image();
   image.src = IconBird;
   element.appendChild(image);
+  console.log(JsonData);
+  console.log(XmlData);
+  console.log(CsvData);
   return element;
 }
 
